da592ef93ec1487a7566aabbf35133ccf8897a9d
CSUYBC <53135822+CSUYBC@users.noreply.github.com>
2020-02-07 23:33:00 +0800

    新增注释

--- rcrs-adf-sample/src/adf/sample/centralized/CommandPickerPolice.java
+++ rcrs-adf-sample/src/adf/sample/centralized/CommandPickerPolice.java
@@ -1,5 +1,5 @@
 package adf.sample.centralized;
-
+//警员指令分配
 import adf.agent.communication.standard.bundle.centralized.CommandPolice;
 import adf.agent.develop.DevelopData;
 import adf.agent.info.AgentInfo;
@@ -18,41 +18,49 @@ import java.util.Collection;
 import java.util.Map;
 
 public class CommandPickerPolice extends CommandPicker {
-
-    private Collection<CommunicationMessage> messages;
-    private Map<EntityID, EntityID> allocationData;
-
-    public CommandPickerPolice(AgentInfo ai, WorldInfo wi, ScenarioInfo si, ModuleManager moduleManager, DevelopData developData) {
+	  //存储所有即将发布的命令
+    private Collection<CommunicationMessage> messages;	
+  //可被分配的警察
+    private Map<EntityID, EntityID> allocationData;		
+    public CommandPickerPolice(AgentInfo ai, WorldInfo wi, ScenarioInfo si, ModuleManager moduleManager, DevelopData developData) {    //构造函数    
         super(ai, wi, si, moduleManager, developData);
         this.messages = new ArrayList<>();
         this.allocationData = null;
     }
 
     @Override
-    public CommandPicker setAllocatorResult(Map<EntityID, EntityID> allocationData) {
+    //指令分配结果
+    public CommandPicker setAllocatorResult(Map<EntityID, EntityID> allocationData) {   
         this.allocationData = allocationData;
         return this;
     }
 
     @Override
-    public CommandPicker calc() {
+    //获取指令算法
+    public CommandPicker calc() {  
         this.messages.clear();
         if(this.allocationData == null) {
             return this;
         }
-        for(EntityID agentID : this.allocationData.keySet()) {
+        //获取每个agent
+        for(EntityID agentID : this.allocationData.keySet()) {   
             StandardEntity agent = this.worldInfo.getEntity(agentID);
-            if(agent != null && agent.getStandardURN() == StandardEntityURN.POLICE_FORCE) {
-                StandardEntity target = this.worldInfo.getEntity(this.allocationData.get(agentID));
+            //agent存在
+            if(agent != null && agent.getStandardURN() == StandardEntityURN.POLICE_FORCE) {  
+            	  //分配任务
+                StandardEntity target = this.worldInfo.getEntity(this.allocationData.get(agentID));  
                 if(target != null) {
-                    if(target instanceof Area) {
+                	  //目标地点存在
+                    if(target instanceof Area) {  
                         CommandPolice command = new CommandPolice(
                                 true,
                                 agentID,
                                 target.getID(),
+                            	//自主选择
                                 CommandPolice.ACTION_AUTONOMY
                         );
-                        this.messages.add(command);
+                      //添加command至message
+                        this.messages.add(command);	
                     }
                 }
             }
